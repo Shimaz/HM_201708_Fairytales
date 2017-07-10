c36a22c0b348b07239dd1afa5fb07337dc4d8e79
shimaz <shimaz.bsh@gmail.com>
2017-07-10 13:17:23 +0900

    added basic activities

---
+++ app/src/main/java/kr/tangomike/hm_201708_fairytales/DataCollection.java
@@ -0,0 +1,66 @@
+package kr.tangomike.hm_201708_fairytales;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by shimaz on 2017-07-10.
+ */
+
+public class DataCollection extends Application {
+    private int ticktime;
+    private static final int RESET_TIME = 60;
+    private boolean isTicking = false;
+    private Handler mHandler;
+
+
+    public void onCreate(){
+        super.onCreate();
+        ticktime = 0;
+        isTicking = false;
+
+        // TODO : broadcast timer message
+
+        mHandler = new Handler(){
+            public void handleMessage(Message msg){
+
+                if(isTicking) ticktime++;
+
+                if(ticktime < RESET_TIME){
+                    mHandler.sendEmptyMessageDelayed(0, 1000);
+                    android.util.Log.i("shimaz", "" + ticktime);
+                }else if(ticktime >= RESET_TIME){
+                    ticktime = 0;
+                    isTicking = false;
+                    mHandler.removeMessages(0);
+
+                    Intent intent  = new Intent("shimaz.restart");
+                    sendBroadcast(intent);
+                }
+
+            }
+
+        };
+
+
+
+    }
+
+    public void resetTimer(){
+        ticktime = 0;
+    }
+
+    public void stopTick(){
+        isTicking = false;
+        mHandler.removeMessages(0);
+    }
+
+    public void startTick(){
+        ticktime = 0;
+        isTicking = true;
+        mHandler.sendEmptyMessageDelayed(0, 1000);
+    }
+
+}
---
+++ app/src/main/java/kr/tangomike/hm_201708_fairytales/PageActivity.java
@@ -0,0 +1,38 @@
+package kr.tangomike.hm_201708_fairytales;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class PageActivity extends Activity {
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            finish();
+        }
+    };
+
+    private IntentFilter mFilter = new IntentFilter("shimaz.restart");
+    private DataCollection dc;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_page);
+        registerReceiver(mReceiver, mFilter);
+    }
+
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy(); 
+        unregisterReceiver(mReceiver);
+    }
+}
