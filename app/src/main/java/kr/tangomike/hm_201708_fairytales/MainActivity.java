9d049aba3141eb3d658a4e6350acb8c93b03ad18
shimaz <shimaz.bsh@gmail.com>
2017-07-12 15:52:02 +0900

    commit 0712 01

--- app/src/main/java/kr/tangomike/hm_201708_fairytales/MainActivity.java
+++ app/src/main/java/kr/tangomike/hm_201708_fairytales/MainActivity.java
@@ -1,13 +1,39 @@
 package kr.tangomike.hm_201708_fairytales;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // finish();
+
+        }
+    };
+
+    private IntentFilter mFilter = new IntentFilter("shimaz.restart");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(mReceiver, mFilter);
+
+    }
+
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
     }
 }
